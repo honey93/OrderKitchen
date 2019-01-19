0888c807c3f2aea1727aa6ca3439bfb1d87bd694
Honey Thakuria <honeythakuria@Honeys-MacBook-Pro.local>
2019-01-19 18:59:22 +0530

    Documentation added for UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import PlaceOrder from "./main/PlaceOrder";
 import UpdatePredicted from "./main/UpdatePredicted";
 import Kitchen from "./main/Kitchen";
 
+/*The <Route> component is the main part of React Router. Anywhere that you want to only render content based on the location’s pathname, you should use a <Route> element. */
+
+/* The Route component expects a path prop, which is a string that describes the pathname that the route matches */
+
+/* The <Switch> will iterate over routes and only render the first one that matches the current pathname */
+
 class App extends Component {
   render() {
     return (
@@ -14,7 +20,7 @@ class App extends Component {
         <Header />
         <Switch>
           <Route exact path="/" component={PlaceOrder} />
-          <Route path="/updatepredicted" component={UpdatePredicted} />
+          <Route path="/updatepredicted" component={UpdatePredicted} />  
           <Route path="/kitchen" component={Kitchen} />
         </Switch>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+/* Using BrowserRouter to handle dynamic requests from Express Server */
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+/* Router components expects to only contain one child component that's why combining everything in App and importing the same */ 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter> 
     <App />
   </BrowserRouter>,
   document.getElementById("root")
